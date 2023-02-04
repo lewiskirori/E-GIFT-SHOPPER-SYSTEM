f2c61a39371e235254bc1badeff764ef05050328
Lewis K Njenga <88181151+lewiskirori@users.noreply.github.com>
2023-02-04 01:50:10 -0800

    About update view

--- about.php
+++ about.php
@@ -4,15 +4,26 @@
             About Us
         </title>
         <link rel="stylesheet" type="text/css" href="about.css">
+        <link rel="shortcut icon" href=" slider-images-1 (1).jpg "> 
     </head>
+    <!---Navigation--->
+    <ul>
+             <button><a href="index.php"><svg xmlns="http://www.w3.org/2000/svg" width="46" height="36"  class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+</svg> </a></button>
+    </ul>
+    
+    <!--body-->
     <body>
         <section class="about">
+            <!-- other content -->
+
             <div class="about-text">
                 <img src="logo-images\brand-img.jpg">
                 <h1>About Us</h1>
                 <h5>System <span>Information</span></h5>
-                <p>This is an <strong>E-GIFT SHOPPER SYSTEM</strong> . It has been developed by <strong>lewiskirori </strong> using <strong>HTML, CSS, PHP , JAVASCRIPT </strong>&amp; <strong>MySQL.</strong> The system has been realized with more interactive and responsive design features making the website accessible to all users, regardless of their device & zone areas.</p>
-                <a href="contact.html"><button type="button"> 💬Let's Talk</button></a>
+                <p>This is an <strong>E-GIFT SHOPPER SYSTEM</strong> . It has been developed by <strong>coldcode </strong></strong> The system has been realized with more interactive and responsive design features making the website accessible to all users, regardless of their device & access areas.</p>
+                <a href="contact.shtm"><button type="button"> 💬Let's Talk</button></a>
             </div>
         </section>
 
@@ -21,8 +32,8 @@
         <!----text------->
         <div class="brand-text">
             <h3>Our Brand</h3>
-            <h4>E-Gift shopper 2022 </h4>
-            <p>We Manage online gift purchases. This allows shoppers to search for and book gifts in their comfort zone areas. Shipping cost is also payable. We help you order the gift you want at a convenient time.
+            <h4>E-Gift shopper <?php echo date ('Y'); ?> </h4>
+            <p>We Manage online gift purchases. This allows shoppers to search for and book gifts in their comfort-zone areas. Shipping cost is also payable. We help you order the gift you want at a convenient time.
             </p>
         </div>
         <!---img------>
