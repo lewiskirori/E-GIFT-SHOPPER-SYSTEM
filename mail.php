dcc44627cf3b04ed2b9b2e3be86e879546232512
Lewis K Njenga <88181151+lewiskirori@users.noreply.github.com>
2023-02-04 01:58:38 -0800

    mail scheduler

--- mail.php
+++ mail.php
@@ -1,8 +1,8 @@
 <?php
-    $receiver = "papilocsta19@gmail.com";
+    $receiver = "```";
     $subject  = "New Gift Notification..!";
     $body     = "Hello, there!...This is an email to inquire newest gifts from the shop.";
-    $sender   = "From: coldcode703@gmail.com";
+    $sender   = "From: ```";
 
     if(mail($receiver, $subject, $body, $sender)){//php function send mail
         echo "Email sent successfully to $receiver !" ;
