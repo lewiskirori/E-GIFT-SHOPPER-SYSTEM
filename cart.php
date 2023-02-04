0fbce18b7965c74b27d843d899f3698246c1e709
Lewis K Njenga <88181151+lewiskirori@users.noreply.github.com>
2023-02-04 02:12:57 -0800

    shopping cart changes

--- cart.php
+++ cart.php
@@ -1,10 +1,17 @@
+<?php session_start();
+if(empty($_SESSION['id'])):
+    header('Location:index.php');
+endif;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href=" logo-images\gift-shop-logo-design-vector-34887709.jpg "> 
+    <link rel="shortcut icon" href=" logo-images\gift-shop-logo-design-vector-34887709.jpg ">
+        
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
@@ -15,23 +22,26 @@
 <body>
    <!-----------------main section---------------->
    <section class="main">
+       
+       <!-------logout------------------------->
+    <a href="logout process.php"><div style="float:right"><button><i class="fa fa-power-off" aria-hidden="true"></i>LogOut</button></div></a>
 
     <!-------logo----------->
-        <div class="logo"><a href="cart.php"><font>Shopping </font>Cart💲🛒</a></div></div>
+        <div class="logo"><a href="cart.php"><font>Shopping </font>Cart<i class="fa fa-opencart"></i></a></div></div>
   
 
   <!------navigation-bar------------->
   <ul>
-    <li ><a href="home.php">🔙Go Back</a></li>
-    <li id="home"><a href="index.php">🏪Home</a></li>
-    <li><a href="contact.php">☎️Contact Us</a></li>
-    <li class="cart"><a href="cart.php"><i class='fas fa-gift' style='font-size:18px'>🛒Your Cart ~ <span>0</span></i></a>
+    <li ><a href="home.php"><i class="fa fa-arrow-left"></i> Back</a></li>
+    
+    <li><a href="contact.shtm"><i class="fa fa-phone"></i> Contact Us</a></li>
+    <li class="cart"><a href="cart.php"><i class="fa fa-opencart"></i> Your Cart ~ <span>0</span></a>
         
     </li>
   </ul>
 
   <!----------------------------------functional-search-box---------------------------------------->
-<input type="search" value="" class="search-bar" id="search" placeholder="Search Bar" onchange="openPage()">
+<input type="search" value="" class="search-bar" id="search" placeholder="Search Gift, Category, ..." onchange="openPage()">
         <div id="navigation"></div>
         <script>
             function openPage(){
