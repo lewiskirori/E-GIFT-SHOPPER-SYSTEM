f8a9c933910c11c2dd383d1e4cf0227546a0aecc
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-02-10 06:45:45 -0800

    add checkout nav

--- cart.php
+++ cart.php
@@ -10,13 +10,22 @@ endif;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="shortcut icon" href=" logo-images/cart-logo.png ">
+    <link rel="shortcut icon" href=" logo-images/cart-logo.png ">
         
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <!--Inter UI font-->
+        <link href="https://rsms.me/inter/inter-ui.css" rel="stylesheet">
+    
+        <!--vendors styles-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+        <!-- Bootstrap CSS / Color Scheme -->
+        <link rel="stylesheet" href="css/default.css" id="theme-color">
     <link rel="stylesheet" type="text/css" href="cart.css"/>
-    <title>Cart</title>
+    <title>Gift Shopping - Cart</title>
 
 </head>
 <body>
@@ -41,7 +50,7 @@ endif;
   </ul>
 
   <!----------------------------------functional-search-box---------------------------------------->
-<input type="search" value="" class="search-bar" id="search" placeholder="Search Gift, Category, ..." onchange="openPage()">
+<input type="search" value="" class="search-bar" id="search" placeholder="Search Gift, Category ..." onchange="openPage()">
         <div id="navigation"></div>
         <script>
             function openPage(){
@@ -79,11 +88,21 @@ endif;
             
         </div>
     </div>
+    
+    <!----------------------scroll to top------------------------>
+            <div class="scroll-top">
+                <i class="fa fa-angle-up" aria-hidden="true"></i>
+            </div>
+            
 <script src="main.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-
-
-
+ <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.7.3/feather.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+    <script src="js/scripts.js"></script>
 
 </section>
 </body>
