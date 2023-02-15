69e22c3bdb61dca4c9460e7d3150649c6b854252
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-02-15 04:10:12 -0800

    Update tags

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
     </head>
     <!---Navigation--->
     <ul>
-             <button><a href="index.php"><svg xmlns="http://www.w3.org/2000/svg" width="46" height="36"  class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+             <button><a href="\"><svg xmlns="http://www.w3.org/2000/svg" width="46" height="36"  class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
 </svg> </a></button>
     </ul>
@@ -19,7 +19,7 @@
             <!-- other content -->
 
             <div class="about-text">
-                <img src="logo-images\brand-img.jpg">
+                <a href="\"><img src="logo-images/EGSS-LoGo.png"></a>
                 <h1>About Us</h1>
                 <h5>System <span>Information</span></h5>
                 <p>This is an <strong>E-GIFT SHOPPER SYSTEM</strong> . It has been developed by <strong>coldcode </strong></strong> The system has been realized with more interactive and responsive design features making the website accessible to all users. We Manage online gift purchases. This allows shoppers to search for and order gifts effectively. Shipping cost is also payable. We help you order the gift you want at the best convenient time.</p>
