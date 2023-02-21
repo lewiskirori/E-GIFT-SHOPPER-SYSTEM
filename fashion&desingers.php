0b572096a871cfa4d836df9cdad7fcfeddca6bcf
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-02-21 02:26:39 -0800

    Update fashion&desingers.php

--- fashion&desingers.php
+++ fashion&desingers.php
@@ -311,9 +311,7 @@ endif;
             </div>       
         <!------btn------->
         <input type="submit" name="subscribe"value="Subscribe" class="subscribe-btn">
-            </div>
-
-            
+            </div>   
     </section>
 
 
