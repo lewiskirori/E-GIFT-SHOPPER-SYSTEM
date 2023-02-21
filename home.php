7132a9898bc50b96e9b5dd156fba30c1c44fb774
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-02-21 04:32:42 -0800

    Update home.php

--- home.php
+++ home.php
@@ -10,7 +10,7 @@ endif;
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="logo-images/EGSS-LoGo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
