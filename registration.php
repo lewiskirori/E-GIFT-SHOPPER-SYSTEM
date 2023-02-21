e8d66acfeb06dc9b78d38b51dfe006847048913c
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-02-21 04:31:44 -0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -13,7 +13,7 @@ require_once('config.php');
      
      <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="shortcut icon" href=" logo-images\e-letter-gift-shop-creative-lettering-sign-logo-design-template_657888-125.jpg "> 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
