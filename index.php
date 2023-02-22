9a311a65b91e524d9aa7b53a258c32126a2912e7
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-02-22 01:33:34 -0800

    Update index.php

--- index.php
+++ index.php
@@ -864,7 +864,7 @@
         </section>
 
     <!-----copyright------------------------->
-    <a href="#copyright" class="copyright">&copy; 2021-<?php echo date ('Y'); ?> E-Gift Shopper | All Rights Reserved.</a>
+    <a href="#copyright" class="copyright">&copy; 2021 - <?php echo date ('Y'); ?> E-Gift Shopper | All Rights Reserved.</a>
     
     
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
