d51fb521c4ec53ab9b98dc9fd421e2d6c239cd7d
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-03-22 05:35:30 -0700

    Update about.php

--- about.php
+++ about.php
@@ -11,16 +11,14 @@
     </head>
     <!---Navigation--->
     <ul>
-             <button><a href="\"><svg xmlns="http://www.w3.org/2000/svg" width="46" height="36"  class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-</svg> </a></button>
+        <button><a href="\"><svg xmlns="http://www.w3.org/2000/svg" width="46" height="36"  class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/></svg> </a></button>
     </ul>
     
     <!--body-->
     <body>
         <section class="about">
             <!-- other content -->
-
             <div class="about-text">
                 <a href="\"><img src="logo-images/EGSS-LoGo.png"></a>
                 <h1>About Us</h1>
@@ -31,20 +29,18 @@
         </section>
 
         <!--------------------------------------our-brand----------------------------------->
-     <section class="our-brand">
-        <!----text------->
-        <div class="brand-text">
-            <h3>Our Brand</h3>
-            <h4>E-Gift shopper <?php echo date ('Y'); ?> </h4>
-            <p>This system has been developed to realize necessities of customers regarding online purchases. It manages all information about shopping, gifts, customers and payment modules.<br> Wonderful Gift Shopping Today!
-            </p>
-        </div>
-        <!---img------>
-        <div class="brand-img">
-        <img src="brand\ourbrand-img.jpg">
-        
-        </div>
+         <section class="our-brand">
+            <!----text------->
+            <div class="brand-text">
+                <h3>Our Brand</h3>
+                <h4>E-Gift shopper <?php echo date ('Y'); ?> </h4>
+                <p>This system has been developed to realize necessities of customers regarding online purchases. It manages all information about shopping, gifts, customers and payment modules.<br> Wonderful Gift Shopping Today!
+                </p>
+            </div>
+            <!---img------>
+            <div class="brand-img">
+            <img src="brand\ourbrand-img.jpg">
+            </div>
         </section>
-
     </body>
 </html>
